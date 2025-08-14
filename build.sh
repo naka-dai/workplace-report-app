57abf819fceb6237f213bad425b2f2dbb1f16849
unknown <1763@domainn.local>
2025-08-14 09:09:35 +0900

    "remove_touch_command"

--- build.sh
+++ build.sh
@@ -19,9 +19,6 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Create database file
-touch database/database.sqlite
-
 # If you have frontend assets (Vite) and public/build is NOT committed:      
 npm install
 npm run build
